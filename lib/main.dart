5cc13985eeccf948cee897bb7c17fed3f4d6190a
Nasrat Nasrati <115137645+Nasrat-Nasrati@users.noreply.github.com>
2025-06-25 11:27:31 +0430

    added the icon chang language bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,48 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'screens/home_screen.dart';
+import 'generated/l10n.dart'; // این فایل توسط flutter_intl ساخته می‌شود
 
 void main() {
   runApp(const AfgNetworkApp());
 }
 
-class AfgNetworkApp extends StatelessWidget {
+class AfgNetworkApp extends StatefulWidget {
   const AfgNetworkApp({Key? key}) : super(key: key);
+
+  @override
+  State<AfgNetworkApp> createState() => _AfgNetworkAppState();
+}
+
+class _AfgNetworkAppState extends State<AfgNetworkApp> {
+  Locale _locale = const Locale('en');
+
+  void _setLocale(Locale newLocale) {
+    setState(() {
+      _locale = newLocale;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // برای حذف نوشته debug
+      debugShowCheckedModeBanner: false,
       title: 'AfgNetwork App',
+      locale: _locale,
+      supportedLocales: S.delegate.supportedLocales,
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'sans', // اگر فونت سفارشی دارید
       ),
-      home: HomeScreen(), // صفحه اصلی
+      home: HomeScreen(setLocale: _setLocale), // ارسال تابع تغییر زبان
     );
   }
 }
